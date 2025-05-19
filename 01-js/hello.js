5cf87851dfc5176e9ac81112fa856989555720c0
rohit <rohitsirsat855@gmail.com>
2025-05-19 10:18:32 +0530

    js first class

---
+++ 01-js/hello.js
@@ -0,0 +1,22 @@
+/**
+ * this class is just a test on js.
+ * just to introduce the light light partss
+ */
+
+console.log("Hello World");
+
+function printChai() {
+  console.log("Hello chaiiii");
+}
+
+function brimgBrush(numOfBrush) {
+  console.log(`get ${numOfBrush} brush `);
+}
+
+function addTwo(num1, nun2) {
+  return num1 + nun2;
+}
+
+printChai();
+brimgBrush(6);
+console.log(addTwo(4, 6));
