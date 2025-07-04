30a53c55d53840a6a25987239a461ff4a344edb8
rohit <rohitsirsat855@gmail.com>
2025-07-04 23:21:09 +0530

    js ftn

---
+++ week-04/05-functions.js
@@ -0,0 +1,37 @@
+function greet(name) {
+  console.log(`Hello ${name}`);
+}
+
+// greet("Hitesh");
+// greet("Piyush");
+
+let globalVar = "I am global";
+
+function modifyGlobal() {
+  globalVar = "I am modified";
+  let blockScopedVar = "I am blocked-scoped";
+  console.log(blockScopedVar);
+}
+
+// modifyGlobal();
+
+let config = (function () {
+  let settings = {
+    theme: "dark",
+  };
+  return settings;
+})();
+
+let person1 = {
+  personsName: "ravi",
+  greet: function () {
+    console.log(`Hello, ${this.personsName}`);
+  },
+};
+
+let person2 = {
+  personsName: "hitesh",
+};
+
+// call() to give the context for "thisssss"
+person1.greet.call(person2);
